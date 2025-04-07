a4bcce01774b7256ec1949565703b3cddb2418a9
profxj <xavier@ucolick.org>
2025-04-07 15:14:01 -0700

    clean up

--- profiler/tests/test_io.py
+++ profiler/tests/test_io.py
@@ -1,15 +1,11 @@
 """ Tests for Spray Gliders """
 
+import pytest
 import glob
 import os
 
-apath = os.getenv('ARCTERX')
-
-import pytest
-
 from profiler import gliderdata
 from profiler import profilers_io
-from profiler import io as p_io
 
 from IPython import embed
 
@@ -26,13 +22,14 @@ def test_write_tojson():
     # Write
     s33.write('tst.json')
 
-# Write profilers
-datafiles = glob.glob(os.path.join(apath, 'gliders/spray/*.mat'))
-sprays = []
-for datafile in datafiles:
-    s = gliderdata.SprayData.from_binned_file(
-        datafile, 'idg', dataset, in_field=True,
-        extra_dict={'adcp_on': False})
-    sprays.append(s)
-
-profilers_io.write_profilers(sprays, 'tst_profilers.json')
+def test_write_profilers():
+    # Load
+    datafiles = glob.glob(os.path.join(apath, 'gliders/spray/*.mat'))
+    sprays = []
+    for datafile in datafiles:
+        s = gliderdata.SprayData.from_binned_file(
+            datafile, 'idg', dataset, in_field=True,
+            extra_dict={'adcp_on': False})
+        sprays.append(s)
+    # Write
+    profilers_io.write_profilers(sprays, 'tst_profilers.json')
