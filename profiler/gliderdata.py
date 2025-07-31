a39445955e0bf2d9af879c33c9bad495ce1e8cd6
profxj <xavier@ucolick.org>
2025-07-31 07:55:37 -0700

    rm calypso

--- profiler/gliderdata.py
+++ profiler/gliderdata.py
@@ -1,72 +1,8 @@
 """ Simple Class to hold glider data """
-import os
-import glob
 
-import numpy as np
-import warnings
-import xarray
-
-from profiler.loading.pymatreader import pymatreader
 from profiler import profilerdata
 
 from IPython import embed
-
-def load_dataset(dataset:str):
-    """
-    Load a dataset based on the provided dataset name.
-
-    Parameters:
-        dataset (str): The name of the dataset to load.
-
-    Returns:
-        list: List of profilerdata.ProfilerData objects
-
-    Raises:
-        ValueError: If the provided dataset is not supported.
-    """
-    if dataset == 'ARCTERX':
-        dfile = os.path.join(
-            os.getenv('OS_SPRAY'), 'ARCTERX', 'arcterx_ctd.mat')
-    elif dataset == 'ARCTERX-Leg2':
-        dfiles = glob.glob(os.path.join(
-            os.getenv('OS_SPRAY'), 'ARCTERX', 'Leg2', '*.mat'))
-
-        pDatas = []
-        for dfile in dfiles:
-            sData =  SprayData.from_binned_file(
-                    dfiles, 'idg', dataset, in_field=True)
-            pDatas.append(
-            )
-    elif dataset == 'Calypso2019':
-        dfile = os.path.join(
-            os.getenv('OS_SPRAY'), 'Calypso', 'calypso2019_ctd.mat')
-    elif dataset == 'Calypso2022':
-        dfile = os.path.join(
-            os.getenv('OS_SPRAY'), 'Calypso', 'calypso2022_ctd.mat')
-        # Loop on mission IDs
-        d = pymatreader.read_mat(dfile)
-        mission_ids = np.unique(d['ctd']['missid'])
-        pDatas = []
-        for missid in mission_ids:
-            sData =  SprayData.from_binned_file(dfile, 'idg', dataset, in_field=False,
-                                                missid=missid)
-
-            # Survey specific cuts
-            maxt = np.max(sData.time)
-            mint = np.min(sData.time)
-            goodt = sData.time < (maxt - 12*24*3600)
-            goodt &= (sData.time > (mint + 3*24*3600))
-            #embed(header='Cutting data 59')
-            sData = sData.profile_subset(np.where(goodt)[0], init=False)
-            # Good velocity data
-            sData = sData.cut_on_good_velocity(init=False)
-            # Save
-            pDatas.append(sData)
-
-    else: 
-        raise ValueError(f"Dataset {dataset} not supported")
-
-    return pDatas
 
 class SprayData(profilerdata.ADCPData):
     """
